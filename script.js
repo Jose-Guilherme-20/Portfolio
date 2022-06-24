7d1e9a251c1b07435e3f89ac3967e162972b46ff
José-Guilherme-20 <jgscosta20@gmail.com>
2022-06-23 23:16:32 -0300

    colocando funçoes nas imagens

--- script.js
+++ script.js
@@ -7,34 +7,26 @@ var habilidade = [
 '<p>HTML</p> <br> <p>É uma linguagem de marcação utilizada na construção de páginas na Web.</p> <br>',
                     '<p>CSS</p> <br> <p>É um mecanismo para adicionar estilo a um documento web.</p> <br>',
                     '<p>JavaScript</p> <br> <p>É uma linguagem de programação. Juntamente com HTML e CSS, é uma das três principais tecnologias da web.</p> <br>',
-                    '<p>ReactJS</p> <br> <p>O React é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web.</p> <br>',
-                    '<p>Styled Components</p> <br> <p>É uma biblioteca (lib) que utiliza o conceito de CSS-in-JS, ou seja, que nos permite escrever códigos CSS dentro do Javascript.</p> <br>',
-                    '<p>Sass</p> <br> <p>É uma linguagem de folhas de estilo. Basicamente o Sass potencializa o CSS.</p> <br>',
+                    '<p>Bootstrap </p> <br> <p> O Bootstrap nada mais é que um framework Front End utilizados por milhares de desenvolvedores web pelo mundo. </p> <br>',
                     '<p>Git</p> <br> <p>É um sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software.</p> <br>',
                     '<p>Github</p> <br> <p>É uma plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git. </p> <br>'
 ]
-
-var a1 = document.querySelector('.texto-descricao')
-function funcao1 (){
-    var ht =document.querySelector('.texto-descricao')
-    ht.innerHTML = habilidade[0]
-    
+var texto = [
+    '*/ Clique em alguma tecnologia */'
+];
+function funcao(){
+    var text = document.getElementById("texto");
+    text.innerHTML = habilidade[0]
+    text.addEventListener('mouseout', onm)
 }
-function funcao2(){
-    if(ht != a1) {
-    var a1 = document.querySelector('.texto-descricao')
-    a1.innerHTML = "<p>" + '* Passe o mouse por cima de alguma habilidade para ler a descrição *' + "</p>"
+
+    function onm (){
+        var text = document.getElementById("texto");
+        text.innerHTML = texto[0]
     }
-}
-function funcao3(){
-    var cs = document.querySelector('.texto-descricao')
-    cs.innerHTML = habilidade[1]
 
-}
-function funcao4(){
-    var a2 = document.querySelector('.texto-descricao')
-    a2.innerHTML = "<p>" + '* Passe o mouse por cima de alguma habilidade para ler a descrição *' + "</p>"
-}
+
+
 function menuShow(){
     var tam = document.getElementsByTagName('header')[0];
       
