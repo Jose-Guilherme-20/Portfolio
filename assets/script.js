740aa796f34cdee7a04733abc6d2f836aee6c2df
José-Guilherme-20 <jgscosta20@gmail.com>
2022-07-10 15:40:22 -0300

    formatando pastas

---
+++ assets/script.js
@@ -0,0 +1,104 @@
+
+
+    
+
+var habilidade = [
+
+'<p>HTML</p> <br> <p>É uma linguagem de marcação utilizada na construção de páginas na Web.</p> <br>',
+                    '<p>CSS</p> <br> <p>É um mecanismo para adicionar estilo a um documento web.</p> <br>',
+                    '<p>JavaScript</p> <br> <p>É uma linguagem de programação. Juntamente com HTML e CSS, é uma das três principais tecnologias da web.</p> <br>',
+                    '<p>Bootstrap </p> <br> <p> O Bootstrap nada mais é que um framework Front End utilizados por milhares de desenvolvedores web pelo mundo. </p> <br>',
+                    '<p>Git</p> <br> <p>É um sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software.</p> <br>',
+                    '<p>Github</p> <br> <p>É uma plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git. </p> <br>'
+]
+var texto = [
+    '*/ Clique em alguma tecnologia */'
+];
+function funcao(){
+    var text = document.getElementById("texto");
+    text.innerHTML = habilidade[0]
+    
+    
+
+}
+function onm (){
+    var text = document.getElementById("texto");
+    text.innerHTML = texto[0]
+}
+
+   
+
+    function funcao1(){
+        var text = document.getElementById("texto");
+        text.innerHTML = habilidade[1]
+        
+    }
+    
+        function onm1 (){
+            var text = document.getElementById("texto");
+            text.innerHTML = texto[0]
+        }
+        function funcao2(){
+            var text = document.getElementById("texto");
+            text.innerHTML = habilidade[2]
+            
+        }
+        
+            function onm2 (){
+                var text = document.getElementById("texto");
+                text.innerHTML = texto[0]
+            }
+
+            function funcao3(){
+                var text = document.getElementById("texto");
+                text.innerHTML = habilidade[3]
+                
+            }
+            
+                function onm3 (){
+                    var text = document.getElementById("texto");
+                    text.innerHTML = texto[0]
+                }
+
+                function funcao4(){
+                    var text = document.getElementById("texto");
+                    text.innerHTML = habilidade[4]
+                    
+                }
+                
+                    function onm4 (){
+                        var text = document.getElementById("texto");
+                        text.innerHTML = texto[0]
+                    }
+
+                    function funcao5(){
+                        var text = document.getElementById("texto");
+                        text.innerHTML = habilidade[5]
+                        
+                    }
+                    
+                        function onm5 (){
+                            var text = document.getElementById("texto");
+                            text.innerHTML = texto[0]
+                        }
+
+
+
+function menuShow(){
+    var tam = document.getElementsByTagName('header')[0];
+      
+    var menuMobile = document.querySelector('.mobile-menu');
+    if(menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "./img responsividade/menu_white_36dp.svg";
+        tam.style.height = 'auto'
+    } else{
+            menuMobile.classList.add('open');
+            document.querySelector('.icon').src = "./img responsividade/close_white_36dp.svg";
+            tam.style.height = 'auto'
+        }
+
+}
+
+
+
--- img1.js
+++
@@ -1,9 +0,0 @@
-function img1() {
-    let img1 = document.getElementById("paragrafo");
-
-    img1.addEventListener("mouseout", mouseout);
-
-    function mouseout() {
-        this.innerHTML = "texto anterior";
-    }
-}
