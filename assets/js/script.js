179d0ab11c6e6eae7e6a686a67fdc4d52633f4fd
José-Guilherme-20 <jgscosta20@gmail.com>
2022-08-17 19:59:11 -0300

    adc scroll no menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,40 @@ function menuShow() {
     tam.style.height = "auto";
   }
 }
+
+// Scroll do Menu
+const hadleScroll = () => {
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+const home = document.getElementById("home");
+home.onclick = hadleScroll;
+
+const hadleHab = () => {
+  window.scroll({
+    top: 660,
+    behavior: "smooth",
+  });
+};
+const hab = document.getElementById("hab");
+hab.onclick = hadleHab;
+
+const hadleSbm = () => {
+  window.scroll({
+    top: 1500,
+    behavior: "smooth",
+  });
+};
+const sbm = document.getElementById("sbm");
+sbm.onclick = hadleSbm;
+
+const hadleProj = () => {
+  window.scroll({
+    top: 2200,
+    behavior: "smooth",
+  });
+};
+const proj = document.getElementById("proj");
+proj.onclick = hadleProj;
